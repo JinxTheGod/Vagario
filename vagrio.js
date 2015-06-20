402f97571e3247bb06bfa637cbc85b477fc4dc3d
JinxTheGod <justthataaron@gmail.com>
2015-06-20 18:25:24 +0100

    vagario.js

---
+++ vagrio.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        ViDe Server Join
+// @namespace   videagario.co.nf
+// @description Joins official ViDe Servers
+// @include     http://agar.io/
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+connect("ws://videagario.ddns.net:9090")
