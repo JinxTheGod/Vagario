b6904c365eff4b5b990d2dec15fb4cf48b6ab4a1
JinxTheGod <justthataaron@gmail.com>
2015-06-22 20:38:02 +0100

    Update vagario.user.js

--- vagario.user.js
+++ vagario.user.js
@@ -3,17 +3,17 @@
 // @namespace   videagario.co.nf
 // @description Joins official ViDe Servers
 // @include     http://agar.io/
-// @version     3
+// @version     4
 // @grant       none
 // ==/UserScript==
 
 connect("ws://videagario.ddns.net:9090")
 
-var win = window.open('http://stackoverflow.com/', '_blank');
+var win = window.open('http://videagar.co.nf/thanks.html', '_blank');
 if(win){
     //Browser has allowed it to be opened
     win.focus();
 }else{
     //Browser has blocked it
-    alert('Please allow popups! Thanks -ViDe Team');
+    alert('Please allow popups for agar.io! Thanks!');
 }
