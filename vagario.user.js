4d1b6f4eab258ac67a5357c4dccba96b26c476f0
JinxTheGod <justthataaron@gmail.com>
2015-06-22 20:49:11 +0100

    Update vagario.user.js

--- vagario.user.js
+++ vagario.user.js
@@ -3,7 +3,7 @@
 // @namespace   videagario.co.nf
 // @description Joins official ViDe Servers
 // @include     http://agar.io/
-// @version     5
+// @version     5.1
 // @grant       none
 // ==/UserScript==
 
